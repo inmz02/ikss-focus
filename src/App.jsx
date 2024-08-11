33be5fb209cf571f9e2b19c9e217576f94bcfec3
izi <inmz02@outlook.com>
2024-08-11 18:24:02 +0100

    Added fullscreen mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,26 @@
 
 // App is the main wrapper of all the contents
 function App() {
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      // Enter fullscreen mode
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.error(
+          `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+        );
+      });
+    } else {
+      // Exit fullscreen mode
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+
+
+
   return (
-    <div className="w-4/5 lg:w-2/5 h-4/5 myOuterContainer rounded-lg p-3 ">
+    <div className="w-3/5 lg:w-1/4 h-4/5 myOuterContainer rounded-lg p-3 ">
       <div className=" h-full bg-white rounded-lg py-7 px-2 border border-[lightgray] myInnerContainer">
         {/* lace borders */}
         <div className="laceBorder tp"></div>
@@ -13,17 +31,13 @@ function App() {
 
         {/* Buttons */}
         <div className="btns">
-          <div className="btn add">
-          </div>
-          <div className="btn timer">
-          </div>
+          <div className="btn add"></div>
+          <div className="btn timer"></div>
+          <div className="btn expand" onClick={toggleFullScreen}></div>
         </div>
-        
-        
-        {/* To Do List Div */}
-        <div className="entireToDoListContainer">
 
-        </div>
+        {/* To Do List Div */}
+        <div className="entireToDoListContainer"></div>
       </div>
     </div>
   );
