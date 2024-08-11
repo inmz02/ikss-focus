4bc9959d37db71ddb59ac5af9837e104e8d97154
izi <inmz02@outlook.com>
2024-08-11 17:56:06 +0100

    Big push

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import { useState } from 'react'
+
+// App is the main wrapper of all the contents
+function App() {
+  return (
+    <div className="w-2/5 h-4/5 myOuterContainer rounded-lg p-3 ">
+      <div className=" h-full bg-white rounded-lg py-7 px-2 border border-[lightgray] myInnerContainer">
+        {/* lace borders */}
+        <div className="laceBorder tp"></div>
+        <div className="laceBorder bt"></div>
+
+        <h1 className="myTitle mt-1 mb-2">Isabelle's Korean Study Space</h1>
+
+        {/* Buttons */}
+        <div className="btns">
+          <div className="btn add">
+            <img src="assets/btn1.png" alt="" />
+          </div>
+          <div className="btn timer">
+            <img src="assets/btn1.png" alt="" />
+          </div>
+        </div>
+        
+        
+        {/* To Do List Div */}
+        <div className="entireToDoListContainer">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
